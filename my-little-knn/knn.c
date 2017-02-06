543dee13ed300d3585abaa7e909e464d75f98a2e
FelSiq <felipe.siqueira@usp.br>
2017-02-06 18:37:21 -0200

    This version of my simple KNN supports small classifications problems. It's not optimized.

--- my-little-knn/knn.c
+++ my-little-knn/knn.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <unistd.h>
 #include <math.h>
+
+#define TASCII_SIZE 128
 
 enum {
 	PROG_NAME,
@@ -28,9 +29,13 @@ long double euclid_dis(unsigned int obs_num, long double *argsA, long double *ar
 	return dis;
 };
 
-static void rec_qck(long double *vector, unsigned int *vecaux, int start, int end){
+static void rec_qck(long double *vector, long double **vaux, char *vecaux, int start, int end){
 	int i = start, j = end;
 	long double pivot = *(vector + (i + j)/2), aux;
+
+	long double *aux2;
+	char aux3;
+
 	while(j >= i){
 		while(i <= end && *(vector + i) < pivot) ++i;
 		while(j >= start && *(vector + j) > pivot) --j;
@@ -39,31 +44,40 @@ static void rec_qck(long double *vector, unsigned int *vecaux, int start, int en
 			*(vector + i) = *(vector + j);
 			*(vector + j) = aux;
 
-			aux = *(vecaux + i);
+			aux2 = *(vaux + i);
+			*(vaux + i) = *(vaux + j);
+			*(vaux + j) = aux2;		
+
+			aux3 = *(vecaux + i);
 			*(vecaux + i) = *(vecaux + j);
-			*(vecaux + j) = aux;
+			*(vecaux + j) = aux3;
 
 			++i;
 			--j;
 		}
 	}
 	if (i < end)
-		rec_qck(vector, vecaux, i, end);
+		rec_qck(vector, vaux, vecaux, i, end);
 	if (j > start)
-		rec_qck(vector, vecaux, start, j);
+		rec_qck(vector, vaux, vecaux, start, j);
 };
 
-void qckSort(long double *vector, unsigned int *aux, size_t size){
-	rec_qck(vector, aux, 0, size - 1);
+void qckSort(void *vector, long double **vaux, char *aux, size_t size){
+	rec_qck((long double *) vector, vaux, aux, 0, size - 1);
 };
 
-long double **get_data(FILE *fdata, size_t *dt_size, unsigned int obs_num){
+long double **get_data(FILE *fdata, char **labels, size_t *dt_size, unsigned int obs_num){
 	long double **dt_matrix = NULL;
 	while(!feof(fdata)){
 		dt_matrix = realloc(dt_matrix, sizeof(long double *) * (1 + *dt_size));
+		*labels = realloc(*labels, sizeof(char *) * (1 + *dt_size));
 		*(*dt_size + dt_matrix) = malloc(sizeof(long double) * obs_num);
 		for(unsigned int i = obs_num; i > 0; --i)
 			fscanf(fdata, "%LF", (*(*dt_size + dt_matrix) + i - 1));
+		fscanf(fdata, "%*c%c", (*labels + *dt_size));
+		#ifdef DEBUG
+			printf("TEST; %c\n", *(*labels + *dt_size));
+		#endif
 		++(*dt_size);
 	};
 	return dt_matrix;
@@ -85,16 +99,16 @@ void destroy_data(long double **data, unsigned int rows){
 
 void knn (unsigned int K, FILE *finput, FILE *fdata, unsigned int obs_num){
 	size_t dt_size = 0;
-	
-	long double **dt_matrix = get_data(fdata, &dt_size, obs_num), 
-	*distances = malloc(sizeof(long double) * dt_size),
-	*obs = malloc(sizeof(long double) * obs_num),
-	sum = 0;
 
-	unsigned int *indeces = malloc(sizeof(unsigned int) * dt_size), i;
+	char *labels = NULL, res_label = 0;	
+	long double **dt_matrix = get_data(fdata, &labels, &dt_size, obs_num), 
+	*distances = malloc(sizeof(long double) * dt_size),
+	*obs = malloc(sizeof(long double) * obs_num);
+
+	unsigned int *freqs = malloc(sizeof(unsigned int) * TASCII_SIZE), i, freq_major = 0;
 
 	while(!feof(finput)){
-		for(i = 0; i < dt_size; *(indeces + i) = i, ++i);
+		for(i = 0; i < TASCII_SIZE; *(freqs + i++) = 0);
 		//Load all data from fdata in a matrix k
 		//begin repeat until feof(finput)k
 		//Get one single case from finput k
@@ -108,14 +122,24 @@ void knn (unsigned int K, FILE *finput, FILE *fdata, unsigned int obs_num){
 		for(i = dt_size; i > 0; 
 			*(distances + i - 1) = euclid_dis(obs_num, obs, *(dt_matrix + i - 1)), 
 			--i);
-		qckSort(distances, indeces, dt_size);
-		printf("group(s): ");
-		for(i = 0; i < K; printf("[%u]", *(indeces + i)), sum += *(distances + i++));
-		printf("\t(total distance: %LF)\n", sum/(1.0 * K));
-		sum = 0;
+		
+		qckSort(distances, dt_matrix, labels, dt_size);
+		
+		for(i = 0; i < K; ++(*(freqs + *(labels + i++))));
+
+		for(i = 0; i < TASCII_SIZE; ++i){
+			if (*(freqs + i) > freq_major){
+				res_label = (char) i;
+				freq_major = *(freqs + i);
+			}
+		}
+		printf("class: %c\n", res_label);
+		freq_major = 0;
+		res_label = 0;
 	};
 	free(obs);
-	free(indeces);
+	free(freqs);
+	free(labels);
 	free(distances);
 	destroy_data(dt_matrix, dt_size);
 };
