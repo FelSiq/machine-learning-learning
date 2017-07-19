db013a11c2a6ff5a8e2875eab2c429e476de9b5e
FelSiq <felipe.siqueira@usp.br>
2017-07-19 19:48:58 -0300

    Compacted polynomial regression code

--- part4-polynomial-regression/pr.py
+++ part4-polynomial-regression/pr.py
@@ -19,34 +19,34 @@ ind_param = np.asmatrix(ind_param[:, -1]).T
 # as it's values can be expressed with a higher-lower sequence:
 # x0 < x1 < x2 < ... < xn
 
-# Build a Univariate Linear Regression Model (just as basis of comparison)
-# Build the Polynomial Linear Regression Model
-from sklearn.linear_model import LinearRegression as skllm_lr
-lin_reg = skllm_lr()
-lin_reg.fit(ind_param, dep_param)
-
-# Compare the two results
 from sklearn.preprocessing import PolynomialFeatures as sklp_pf
-ind_param_poly_degree2 = sklp_pf(degree = 2).fit_transform(ind_param)
-ind_param_poly_degree4 = sklp_pf(degree = 4).fit_transform(ind_param)
-ind_param_poly_degree8 = sklp_pf(degree = 8).fit_transform(ind_param)
-# How to define the correct degree? This must be verified! [x] 
-pol_reg_degree2 = skllm_lr()
-pol_reg_degree4 = skllm_lr()
-pol_reg_degree8 = skllm_lr()
-pol_reg_degree2.fit(ind_param_poly_degree2, dep_param)
-pol_reg_degree4.fit(ind_param_poly_degree4, dep_param)
-pol_reg_degree8.fit(ind_param_poly_degree8, dep_param)
+from sklearn.linear_model import LinearRegression as skllm_lr
+
+# Plot a polynomial prediction curve ========
+def plot_polcurve(ind_param, dep_param, color = 'red', degree = 2):
+	"""
+	Crucial explanations:
+		- new_dataset = dataset of the independent variables with n polynomial x's
+		- poly_regressor = linear polynomial model to the "degree", fitted to the n degree
+	"""
+	# Build the Polynomial Linear Regression Model
+	new_dataset = sklp_pf(degree = degree).fit_transform(ind_param)
+	poly_regressor = skllm_lr()
+	poly_regressor.fit(new_dataset, dep_param)
+	plt.plot(ind_param, poly_regressor.predict(new_dataset), color = color)
 
 # Results ===================================
 import matplotlib.pyplot as plt
 # 	Visualise the univariate linear regressor results
 # + Visualise the polynomial linear regressor reuslts
 plt.scatter(np.asarray(ind_param), dep_param, color = 'blue')
-plt.plot(ind_param, lin_reg.predict(ind_param), color = 'red')
-plt.plot(ind_param, pol_reg_degree2.predict(ind_param_poly_degree2), color = 'green')
-plt.plot(ind_param, pol_reg_degree4.predict(ind_param_poly_degree4), color = 'gray')
-plt.plot(ind_param, pol_reg_degree8.predict(ind_param_poly_degree8), color = 'black')
+# plt.plot(ind_param, lin_reg.predict(ind_param), color = 'red')
+
+plot_polcurve(ind_param, dep_param, 'red', 1) # Linear = Univariate Regression
+plot_polcurve(ind_param, dep_param, 'green', 2)
+plot_polcurve(ind_param, dep_param, 'green', 4)
+plot_polcurve(ind_param, dep_param, 'green', 8)
+
 plt.title('Univariate vs Polynomial Linear Regression')
 plt.xlabel('Level')
 plt.ylabel('Salary')
