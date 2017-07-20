e7a4b75b4c5efe27a6bdb172c48ff77c54513a8a
FelSiq <felipe.siqueira@usp.br>
2017-07-20 20:42:30 -0300

    Added a improved version w/ outlier on Python SVR Code

--- part5-support-vector-regression/svr.py
+++ part5-support-vector-regression/svr.py
@@ -1,26 +1,38 @@
+# ======================================================
 # Get the dataset
 import pandas as pd
 dataset = pd.read_csv('/home/felipe/Documentos/Machine Learning A-Z/Part 2' + 
 	' - Regression/Section 7 - Support Vector Regression (SVR)/Position_Salaries.csv')
+# ======================================================
+# Create a outlier test sample
+test_dataframe = pd.DataFrame({
+	'Position':	['Outlier'],
+	'Level':	[11],
+	'Salary':	[3e+06]
+	})
+# Concatenate the new outlier sample into the original dataset
+dataset = pd.concat([dataset, test_dataframe])
+dataset = dataset.reindex_axis(['Position', 'Level', 'Salary'], axis = 1)
+# ======================================================
+# Split the dependent and independent parameters
 ind_param = dataset.iloc[:, 1:2].values
 dep_param = dataset.iloc[:,  -1].values
-
+# ======================================================
 # No missing values neither categorical data.
-
 # Can't split the dataset into train and test set, due to it's small size
-
+# ======================================================
 # Feature scale is a must in Support Vector Regression Class in Python Package
 from sklearn.preprocessing import StandardScaler as sklp_ss
 ind_scaler = sklp_ss()
-ind_param = ind_scaler.fit_transform(ind_param.reshape(-1, 1))
+ind_param = ind_scaler.fit_transform(ind_param.astype(float).reshape(-1, 1))
 dep_scaler = sklp_ss()
-dep_param = dep_scaler.fit_transform(dep_param.reshape(-1, 1))
-
+dep_param = dep_scaler.fit_transform(dep_param.astype(float).reshape(-1, 1))
+# ======================================================
 # Construct a initial kernel model
 from sklearn.svm import SVR as svr
 regressor = svr(kernel = 'rbf')
-regressor.fit(ind_param, dep_param)
-
+regressor.fit(ind_param, dep_param.ravel())
+# ======================================================
 # Make a predicition
 import numpy as np
 # First, create a numpy array of the wanted input values
@@ -35,7 +47,7 @@ predictions = regressor.predict(input_prediction)
 predictions = dep_scaler.inverse_transform(predictions)
 # One line code: predictions = dep_scaler.inverse_transform(regressor.predict(ind_scaler.transform(np.array([[6.5]]))))
 print predictions
-
+# ======================================================
 # Verify the performance of the SVR model
 import matplotlib.pyplot as plt
 plt.scatter(ind_param, dep_param, color = 'blue')
@@ -44,3 +56,4 @@ plt.title('True values vs Prediction')
 plt.xlabel('Level')
 plt.ylabel('Salary')
 plt.show()
+# ======================================================
