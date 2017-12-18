06054890be405a4aadcabf4018fda6475baf3d9c
FelSiq <felipe.siqueira@usp.br>
2017-12-18 01:18:19 -0200

    Corrected some Perceptron's details.

--- model-implementation/py-perceptron/perceptron.py
+++ model-implementation/py-perceptron/perceptron.py
@@ -5,26 +5,27 @@ import random
 class perceptron:
 	def __init__(self):
 		self.weights = np.array([])
-		self.linearCoefTheta = 0.0
-
-	"""
-	Method used on the fit method. Should not be called outside him. 
-	"""
-	def _updateWeights(self, x, y, trainStep = 0.1):
-		constFactor = y - (np.sum(x * self.weights) + self.linearCoefTheta)
-
-		for i in range(len(self.weights)):
-			self.weights[i] = self.weights[i] + trainStep * (2.0 * x[i] * constFactor)
-		self.linearCoefTheta = self.linearCoefTheta + trainStep * (2.0 * constFactor)
-
-		return y - (np.sum(x * self.weights) + self.linearCoefTheta)
 
 	"""
 	Uses the 'weights' and 'linearCoefTheta', adjusted by a previous 'fit' operation, to
 	predict a output real value of a given query sample.
 	"""
 	def predict(self, query, outThreshold = 0.5):
-		return int((np.sum(query * self.weights) + self.linearCoefTheta) > outThreshold)
+		return int(np.sum(np.concatenate((query, [1.0])) * self.weights) > outThreshold)
+
+	"""
+	Method used on the fit method. Should not be called outside him. 
+	"""
+	def _updateWeights(self, x, y, trainStep = 0.1):
+		xConc = np.concatenate((x, [1.0]))
+
+		# Originally the formula was w[i] = w[i] - transStep(-2.0 * xConc[i] * consFactor),
+		# but it was simplified.
+		constFactor = y - self.predict(x)
+		for i in range(len(self.weights)):
+			self.weights[i] += trainStep * xConc[i] * constFactor
+
+		return constFactor
 
 	"""
 	The Fit method will adjust the 'weights' and the 'linearCoefTheta' of the perceptron.
@@ -35,25 +36,29 @@ class perceptron:
 	Obvious enough, but I'll still gonna say, x is the independent variables of the dataset,
 	and y the dependent/output/label/class value.
 	"""
-	def fit(self, x, y, maxError = 1.0e-15, maxIterations = 500, trainStep = 0.1, showError = False):
+	def fit(self, x, y, maxError = 1.0e-04, maxIterations = 500, trainStep = 0.1, showError = False):
 		# Initial fit setup
-		curError = maxError * 2
-		self.weights = np.array([0.0] * x.shape[1])
+		meanSqrdError = maxError * 2
+
+		self.weights = np.array([0.0] * (x.shape[1] + 1))
 		for i in range(len(self.weights)):
-			self.weights[i] = random.uniform(0.0, 1.0)
-		self.linearCoefTheta = random.uniform(0.0, 1.0)
+			self.weights[i] = random.uniform(-0.5, 0.5)
 		
 		# Perceptron training loop
 		curIteration = 0
-		while abs(curError) > maxError and curIteration < maxIterations:
+		n = x.shape[0]
+		while meanSqrdError > maxError and curIteration < maxIterations:
 			curIteration += 1
-			curError = 0.0
+			meanSqrdError = 0.0
 			
-			for i in range(x.shape[0]):
-				curError += self._updateWeights(x[i], y[i], trainStep)
-			
+			for i in range(n):
+				meanSqrdError += (self._updateWeights(x[i], y[i], trainStep))**2.0
+			meanSqrdError /= n
+
 			if showError:
-				print('I:', curIteration, '\t- curError:', curError)
+				print('i:', curIteration, '\t- meanSqrdError:', meanSqrdError)
+
+		print(self.weights)
 
 
 # Perceptron testing
@@ -65,30 +70,34 @@ if __name__ == '__main__':
 
 	print('AND TESTING:')
 	dataset = pd.read_csv('./test/ANDTruthTable.in', sep=' ')
-	p.fit(dataset.iloc[:,:-1].values, dataset.iloc[:, -1].values)
+	p.fit(dataset.iloc[:,:-1].values, dataset.iloc[:, -1].values, showError = True)
 	ANDQueries = np.array([
-		[-1, -1, -1],
-		[-1, +1, -1],
-		[+1, +1, +1],
-		[-1, +1, +1],
+		[0, 0, 0],
+		[0, 1, 1],
+		[1, 1, 1],
+		[1, 0, 0],
+		[0, 1, 0],
+		[0, 1, 1],
 	])
 
 	for a in ANDQueries:
-		print('Query:', a, '\nOutput:', p.predict(a))
+		print('Query:', a, '\t\tOutput:', p.predict(a))
 
 	print('\nOR TESTING:')
 	dataset = pd.read_csv('./test/ORTruthTable.in', sep=' ')
-	p.fit(dataset.iloc[:,:-1].values, dataset.iloc[:, -1].values)
+	p.fit(dataset.iloc[:,:-1].values, dataset.iloc[:, -1].values, showError = True)
 
 	ORQueries = np.array([
-		[-1, -1, -1],
-		[-1, +1, -1],
-		[+1, +1, +1],
-		[-1, +1, +1],
+		[0, 0, 0],
+		[0, 1, 1],
+		[1, 1, 1],
+		[1, 0, 0],
+		[0, 1, 0],
+		[0, 1, 1],
 	])
 
 	for o in ORQueries:
-		print('Query:', o, '\nOutput:', p.predict(o))
+		print('Query:', o, '\t\tOutput:', p.predict(o))
 
 	# Perceptron is a linear separator. Because of this, it is
 	# incapable of understanding the XOR Truth table.
@@ -97,11 +106,11 @@ if __name__ == '__main__':
 	p.fit(dataset.iloc[:,:-1].values, dataset.iloc[:, -1].values)
 
 	XORQueries = np.array([
-		[-1, -1],
-		[-1, +1],
-		[+1, +1],
-		[-1, +1],
+		[0, 0],
+		[0, 1],
+		[1, 1],
+		[0, 1],
 	])
 
 	for x in XORQueries:
-		print('Query:', x, '\nOutput:', p.predict(x))
+		print('Query:', x, '\t\tOutput:', p.predict(x))
