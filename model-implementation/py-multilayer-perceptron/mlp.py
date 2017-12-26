0032e35fe8b7c8a13d57bb7f82ab296ba2c3f7a6
FelSiq <felipe.siqueira@usp.br>
2017-12-26 21:14:52 -0200

    More steps on mlp implementation.

--- model-implementation/py-multilayer-perceptron/mlp.py
+++ model-implementation/py-multilayer-perceptron/mlp.py
@@ -3,56 +3,89 @@ Information:
 
 
 """
+import pandas as pd
 import numpy as np
 import random
 import math
 
 class mlp:
-	def __init__(self, eta = 0.2, maxError = 1.0e-0, maxIt = 300, delta = 0.5):
-		self.eta = eta
-		self.weights = np.array([])
-		self.outputNum = 0
-		self.hiddenNum = 0
+	def __init__(self, stepSize = 0.2, maxError = 1.0e-05, maxIteration = 300, classThreshold = 0.5):
+		self.stepSize = 0.2
+		self.maxError = 1.0e-05
+		self.maxIteration = 300
+		self.classThreshold = 0.5
+		self.wHidden = np.array([])
+		self.wOutput = np.array([])
+		self.outputLayerSize = 0
+		self.hiddenLayerSize = 0
+		self.inputLayerSize = 0
 
-	def _sigmoid(self, x, lamba = 1.0):
-		return (1 + math.e**(-lamba * x))**(-1.0)
+	def _sigmoid(self, x, Lambda = 1.0):
+		return (1.0 + math.e**(-Lambda * x))**(-1.0)
 
-	def _adjustWeights(self, prediction):
-		# Output Layer
+	def _adjustWeights(self, x, y):
+		# Output layer
+		predict = self.predict(x)
+		for i in range(self.hiddenLayerSize):
+			w[1, i] += self.stepSize * (y - predict) * predict * (1.0 - predict) * ???
 
-
-		# Hidden Layer
+		# Hidden layer
+		for i in range(self.outputLayerSize):
 
 	def predict(self, query):
+		hiddenLayerOutput = np.array([0] * self.hiddenLayerSize)
+		for i in range(self.hiddenLayerSize):
+			hiddenNet = np.sum(self.wHidden[i] * np.concatenate((query, 1.0)))
+			hiddenLayerOutput[i] = this._sigmoid(hiddenNet)
 
+		finalOutput = np.array([0] * self.outputLayerSize)
+		for k in range(self.outputLayerSize):
+			outputNet = np.sum(self.wOutput[i] * np.concatenate((hiddenLayerOutput[i], 1.0)))
+			finalOutput[k] = self._sigmoid(outputNet)
 
-	def fit(self, dataset, hiddenNum = 2):
+		return finalOutput
+
+	def _initWeights(self):
+		for i in range(self.hiddenLayerSize):
+			self.wHidden[i] = [random.random() - 0.5 for j in range(self.inputLayerSize)]
+		for i in range(self.outputLayerSize):
+			self.wOutput[i] = [random.random() - 0.5 for j in range(self.hiddenLayerSize)]
+
+	def fit(self, dataset, hiddenLayerSize = 2):
 		n = dataset.shape[0]
 		x = dataset.iloc[:,:-1].values
 		y = dataset.iloc[:, -1].values
-		self.outputNum = len(set(y))
-		self.hiddenNum = hiddenNum
 
-		self.weights = np.array([[0] * self.hiddenNum, [0] * self.outputNum])
-		for i in self.hiddenNum:
-			self.weights[0, i] = random.uniform(-0.75, 0.75)
-		for i in self.outputNum:
-			self.weights[1, i] = random.uniform(-0.75, 0.75)
+		self.outputLayerSize = math.ceil(math.log(len(set(y)), 2))
+		self.hiddenLayerSize = hiddenLayerSize
+		self.inputLayerSize = dataset.shape[1]
+
+		self._initWeights()
 
 		meanSqrError = self.maxError * 2.0
 		curIteration = 0
-		while meanSqrError > self.maxError and curIteration < self.maxIt:
+		while meanSqrError > self.maxError and curIteration < self.maxIteration:
 			curIteration += 1
 			meanSqrError = 0.0
 
 			for i in range(n):
-				curPrediction = self.predict(x[i])
+				curPrediction = self.predict(x[i], y[i])
 				meanSqrError += self._adjustWeights(y, curPrediction)
 			meanSqrError /= n
 
 
 # Program driver
 in __name__ == '__main__':
+	dataset = pd.read_csv('./dataset/XOR.dat', sep = ' ')
 	mlp = mlp()
+	mlp.fit(dataset)
 
-	mlp.fit()
+	XORQueries = np.array([
+			[1, 0],
+			[0, 0],
+			[0, 1],
+			[1, 1],
+		])
+
+	for q in XORQueries:
+		print('query:', q, 'result:', mlp.predict(q))
