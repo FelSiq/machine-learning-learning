5662463ed4061af46abea6a5bb9741f4034fce41
FelSiq <felipe.siqueira@usp.br>
2018-01-19 01:20:31 -0200

    Finished and tested Naive Bayes Python imp.

--- model-implementation/py-naive-bayes/nb.py
+++ model-implementation/py-naive-bayes/nb.py
@@ -10,7 +10,7 @@ First call 'fit' method, then you can call 'predict' method whenever you want.
 class naiveBayes:
 	def __init__(self):
 		self.classProbs = dict()
-		self.colProbs = np.array([])
+		self.colProbs = dict()
 		self.classes = np.array([])
 		self.queryPropClass = dict()
 
@@ -19,24 +19,42 @@ class naiveBayes:
 		self.classFreqs = pd.value_counts(y)
 		self.classProbs = dict(self.classFreqs / np.sum(self.classFreqs))
 		self.classFreqs = dict(self.classFreqs)
-		self.classes = list(classFreqs.keys())
+		self.classes = list(self.classFreqs.keys())
 
-		# Possible values possibilities
+		# Compute all possibilities
 		colNum = x.shape[1]
 		for c in self.classes:
 			curData = x[y == c]
+			self.colProbs[c] = np.array([dict() for k in range(colNum)])
 			for j in range(colNum):
 				self.colProbs[c][j] = dict(pd.value_counts(curData[:,j]) / self.classFreqs[c])
 
-	def predict(self, query):
-		self.queryPropClass = {key : 1.0 for key in self.classes}
+		return self
 
+	def predict(self, query, crisp=True, normalizeProb=True):
+		self.queryPropClass = {key : self.classProbs[key] for key in self.classes}
+
+		# Compute probability using Naive Bayes Theorem for every class
 		for c in self.classes:
 			for i in range(len(query)):
 				queryVal = query[i]
 				self.queryPropClass[c] *= self.colProbs[c][i][queryVal]
 
-		return max(queryPropClass)
+		# Probability normalization
+		if (normalizeProb):
+			total = sum(self.queryPropClass.values())
+			self.queryPropClass = {key:val/total for key, val in self.queryPropClass.items()}
+		
+		# If crisp is true, only the most probable class will be returned...
+		if (crisp):
+			return max(self.queryPropClass)
+		# If false, return a dictionary with the probability of each class.
+		return self.queryPropClass
 
 # Program driver
 if __name__ == '__main__':
+	dataset = pd.read_csv('tenis.dat', sep=',')
+
+	model = naiveBayes().fit(x = dataset.iloc[:,1:-1].values, y = dataset.iloc[:,-1].values)
+
+	print(model.predict(['ensolarado', 'fria', 'alta', 'forte']))
