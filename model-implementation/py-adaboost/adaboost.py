be174487a6f190eea0cf2e79084f434063f44cb2
FelSiq <felipe.siqueira@usp.br>
2018-02-17 23:08:35 -0200

    Adaboost hotfix.

--- model-implementation/py-adaboost/adaboost.py
+++ model-implementation/py-adaboost/adaboost.py
@@ -2,58 +2,52 @@ import numpy as np
 import math
 
 """
-This is a early version of adaboost. It does not tries classifiers by itself already,
-it's just a prototype.
+This is an early version of adaboost. It does not induce classifiers by itself,
+it just takes the misclassified instances of each one and return the vote power
+of each classifier on the final strong classifier.
 """
 
 class adaboost:
-	def _calcAlpha(self, error):
-		return 0.5 * math.log((1.0 - error) / error)
+	def _calcAlpha(self, error, ep=1.0e-07):
+		return 0.5 * math.log(ep + (1.0 - error) / (error + ep))
 
-	def _updateWeight(self, weight, error, misclassified):
-		return weight * 0.5 * 1.0/(error if misclassified else (1.0 - error))
+	def _updateWeight(self, weight, error, misclassified, ep=1.0e-7):
+		return weight * 0.5/(ep + (error if misclassified else (1.0 - error)))
 
-	def emsemble(self, instNum, misclassified, maxIteration=4, halfwayError=True, verbose=False):
+	def emsemble(self, instNum, misclassified, maxIteration=10, verbose=False):
 		weights = np.array([1.0/instNum] * instNum)
 
 		alphas = {classifier : 0.0 for classifier in misclassified}
 
 		it = 0
 		minError = math.inf
-		while it < maxIteration and minError != 0.5:
+		while it < maxIteration:
 			it += 1
 
-			minError = -math.inf if halfwayError else math.inf
+			minError = math.inf
 			bestClassifier = None
-
 
 			for classifier in misclassified:
 				curError = sum(weights[misclassified[classifier]])
 
-				if halfwayError:
-					curError = abs(curError - 0.5)
-
-				# if (not halfwayError and curError < minError) or (halfwayError and curError >= minError):
-				if halfwayError ^ (curError < minError):
+				if curError < minError:
 					minError = curError
 					bestClassifier = classifier
 
-			if not halfwayError or minError != 0.5:
-				if verbose:
-					print('Round', it, '\tchoosen classifier:', bestClassifier, '\terror:', minError)
-				alphas[bestClassifier] += self._calcAlpha(minError)
+			if verbose:
+				print('Round', it, '\tchoosen classifier:', bestClassifier, '\terror:', minError)
+			alphas[bestClassifier] += self._calcAlpha(minError)
 
-				# Update weights
-				for n in range(instNum):
-					weights[n] = self._updateWeight(weights[n], minError, n in misclassified[bestClassifier])
-			elif verbose:
-				print('Reached min error of 0.5. Stopping boosting.')
+			# Update weights
+			for n in range(instNum):
+				weights[n] = self._updateWeight(weights[n], minError, n in misclassified[bestClassifier])
 
 		return alphas
 
 
 if __name__ == '__main__':
-	instNum = 8
+	instNum = 10
 	misclassified = {'A': [1,5,6], 'B': [3,4], 'C': [0,4,6], 'E': [2,3,6]}
-	strongerClassifier = adaboost().emsemble(instNum, misclassified, halfwayError=False, verbose=True)
+	# misclassified = {'B': [0,4,5,8], 'D': [0,3,6,7], 'F': [1,6], 'I': [1,2,7,9]}
+	strongerClassifier = adaboost().emsemble(instNum, misclassified, verbose=True, maxIteration=10)
 	print(strongerClassifier)
