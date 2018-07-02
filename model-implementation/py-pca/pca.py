0ccc41e84f1448cdb90c3ef8e14cacf5f18e87a8
FelSiq <felipe.siqueira@usp.br>
2018-07-02 01:01:31 -0300

    Updated pca code

--- model-implementation/py-pca/pca.py
+++ model-implementation/py-pca/pca.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import sys
 
 """
@@ -14,7 +16,17 @@ class Pca:
 		self.eigenvalues=None
 		self.eigenvectors=None
 
-	def transform(self, filepath, normalize=True, sep=","):
+	def _plot_line(self, intercept, slope, vertical=False):
+		axes=plt.gca()
+		if not vertical:
+			x_vals=np.array(axes.get_xlim())
+			y_vals=intercept + slope * x_vals
+		else:
+			x_vals=np.array([0, 0])
+			y_vals=intercept + np.array(axes.get_ylim())
+		plt.plot(x_vals, y_vals, '--')
+
+	def transform(self, filepath, normalize=True, sep=",", plot=True):
 		dataset = pd.read_csv(filepath, sep=sep)
 		
 		# Normalize the dataset
@@ -26,18 +38,39 @@ class Pca:
 		#		-> Calc the correlation matrix
 
 		# Center the dataset values to the origin
-		dataset -= dataset.mean()
+		dataset -= dataset.mean(axis=0)
 
 		# Get transformation matrix
 		if normalize:
-			dataset /= dataset.std()
+			dataset /= dataset.std(axis=0)
 			mat = dataset.corr()
 		else:
 			mat = dataset.cov()
 
 		# Now compute eigen-stuff of computed matrix
-		
-		
+		self.eigenvalues, self.eigenvectors=np.linalg.eig(mat)
+		plot &= len(self.eigenvectors) == 2
+
+		if plot:
+			plt.subplot(1, 2, 1)
+			plt.plot(dataset.iloc[:,0].values, dataset.iloc[:,1].values, "o")
+			slope_pca_1=self.eigenvectors[0,1]/self.eigenvectors[0,0]
+			slope_pca_2=self.eigenvectors[1,1]/self.eigenvectors[1,0]
+			self._plot_line(0, slope_pca_1)
+			self._plot_line(0, slope_pca_2)
+		else:
+			print("Warning: cannot plot with dimensions diff than 2.")
+
+		# Linear transformation from eigenvectors
+		# to rotate the data to the eigenvectors basis
+		dataset=pd.DataFrame(np.matmul(dataset, self.eigenvectors))
+	
+		if plot:
+			plt.subplot(1, 2, 2)
+			plt.plot(dataset.iloc[:,0].values, dataset.iloc[:,1].values, "o")
+			self._plot_line(0, 0)
+			self._plot_line(0, 0, vertical=True)
+			plt.show()
 
 		return dataset
 
@@ -49,8 +82,18 @@ class Pca:
 			raise Exception("No eigenvalues of previous transformation"+\
 				" found nor eigenvalues parameter was set.")
 
-		if not isinstance(dataset, pd.DataFrame):
-			raise TypeError("Dataset parameter must be a pandas.DataFrame instance.")
+		new_dataset=pd.DataFrame.copy(dataset)
+
+		# Normalize eigenvalues to calculate the percentage
+		# of information retained while removing less important
+		# columns
+		e_vals_norm=self.eigenvalues/sum(self.eigenvalues)
+
+		cur_information=1.0
+		while cur_information > information:
+			break
+	
+		return new_dataset
 
 
 """
@@ -78,16 +121,15 @@ if __name__ == "__main__":
 	# New system basis
 	eigenvectors=model.eigenvectors
 	print("Eigenvectors (new system basis):\n", eigenvectors)
-	
 
 	# The columns with smallest eigenvalues are cut off first
 	eigenvalues=model.eigenvalues
 	print("Eigenvalues (used to feature selection):\n", eigenvalues)
 
 	# Interesting fact: sum(eigenvalues) = sum(variance of all columns)
-	print("Sum of eigenvalues is the sum of columns variance:\n",
-		sum(eigenvalues), sum(t1_dataset.var()))
-	
+	print("Sum of eigenvalues is the sum of columns unbiased variance:\n",
+		sum(eigenvalues), sum(t1_dataset.var(ddof=1, axis=0)))
+
 	# Select features mantaining a minimum of 0.95 of 
 	# the dataset information
 	t2_dataset=model.feature_selection(t1_dataset, \
