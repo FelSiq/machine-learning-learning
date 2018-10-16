20fe50f6fbda89c0602be7f92faf22d89ca71f24
FelSiq <felipe.siqueira@usp.br>
2018-10-16 03:52:06 -0300

    Added method to calculate cophenetic correlation

--- model-implementation/py-hierarchical-clustering/hierclus.py
+++ model-implementation/py-hierarchical-clustering/hierclus.py
@@ -4,9 +4,10 @@
 	Channel: ML4U
 """
 
-from numpy import array, zeros, inf, median
+from numpy import array, zeros, inf, median, corrcoef
 from pandas import read_csv
 from scipy.special import binom
+from scipy.stats.stats import pearsonr
 
 class Hierclus():
 
@@ -89,8 +90,88 @@ class Hierclus():
 		# Default case
 		return Hierclus.__linkaverage__
 
-	def run(dataset, criterion="single", min_cluster_num=1):
+	def __recfillcoph__(\
+		cophenetic_mat, 
+		cur_node_id, 
+		cur_node_edges, 
+		cluster_heights):
+
+		visited_nodes = set()
+
+		for son in cur_node_edges:
+			if type(cur_node_edges[son]) != type({}):
+				visited_nodes.update({cur_node_edges[son]})
+
+			else:
+				visited_nodes.update(\
+					Hierclus.__recfillcoph__(\
+						cophenetic_mat, 
+						son,
+						cur_node_edges[son], 
+						cluster_heights))
+
+		for node_a in visited_nodes:
+			for node_b in visited_nodes - {node_a}:
+				col = max(node_a, node_b)
+				row = min(node_a, node_b)
+				array_ind = (col * (col - 1))//2 + row
+				
+				if cophenetic_mat[array_ind] is None:
+					cophenetic_mat[array_ind] = cluster_heights[cur_node_id]
+
+		return visited_nodes
+
+	def __copheneticmat__(cluster_tree, cluster_heights):
+		"""
+			Let be the "Dendrogrammatic distance" of two instances
+			"a" and "b" the height of the cluster at which "a" and
+			"b" are first tied together.
+		"""
 	
+		head_node_id = max(cluster_heights)
+	
+		cophenetic_mat = array([None] * int(binom(dataset.shape[0], 2)))
+		Hierclus.__recfillcoph__(\
+			cophenetic_mat, 
+			head_node_id,
+			cluster_tree[head_node_id], 
+			cluster_heights)
+
+		return cophenetic_mat
+
+	def cophenetic_corr(dataset, cluster_tree, cluster_heights):
+		"""
+			The cophenetic correlation measures how well
+			the given dendrogram (in this case, the "cluster_tree")
+			represents the data dispersion.
+		"""
+
+		cophenetic_mat = Hierclus.__copheneticmat__\
+			(cluster_tree, cluster_heights)
+		
+		dist_mat = array([None] * int(binom(dataset.shape[0], 2)))
+		for inst_a_ind in range(dataset.shape[0]-1):
+			for inst_b_ind in range(inst_a_ind+1, dataset.shape[0]):
+				dist_mat[(inst_b_ind * (inst_b_ind - 1))//2 + inst_a_ind] \
+					= Hierclus.__euclideandist__(\
+						dataset[inst_a_ind,:], dataset[inst_b_ind,:])
+
+		return pearsonr(cophenetic_mat, dist_mat)[0]
+
+	def run(dataset, 
+		criterion="single", 
+		min_cluster_num=1, 
+		cophenetic_corr=False,
+		warnings=True):
+
+		if min_cluster_num < 1:
+			print("Error: min_cluster_num must be >= 1.")
+			return None
+	
+		if warnings and cophenetic_corr and min_cluster_num > 1:
+			print("Warning: \"cophenetic_corr\" needs", 
+				"\"min_cluster_num\" equals to 1.")
+
 		clustering_metric = Hierclus.__choosecriterion__(criterion)
 		if clustering_metric is None:
 			return None
@@ -122,7 +203,7 @@ class Hierclus():
 			supergroup_ids = tuple(cluster_tree.keys())
 
 			k = 0
-			for i in range(num_supergroups):
+			for i in range(num_supergroups-1):
 				supergroup_A_id = supergroup_ids[i]
 				for j in range(i+1, num_supergroups):
 					supergroup_B_id = supergroup_ids[j]
@@ -178,6 +259,11 @@ class Hierclus():
 			"cluster_num" : len(cluster_tree),
 		}
 
+		if cophenetic_corr and min_cluster_num == 1:
+			ans["cophenetic_corr"] = \
+				Hierclus.cophenetic_corr(\
+					dataset, cluster_tree, group_heights)
+
 		return ans
 
 if __name__ == "__main__":
@@ -188,6 +274,7 @@ if __name__ == "__main__":
 			"\t[-label class_label]",
 			"\t[-sep data_separator, default to \",\"]",
 			"\t[-n minimal_number_of_clusters, default is 1]",
+			"\t[-coph, enable to calculate the cophenetic correlation]",
 			"\nWhere <linkage_type> must be one of the following:",
 			"\tsingle: connect the nearest groups using the nearest pair of instances.",
 			"\tcomplete: connect the nearest groups using the farthest pair of instances.",
@@ -195,6 +282,8 @@ if __name__ == "__main__":
 			"\tmedian: connect the nearest groups using the median distance between the instances.", 
 			sep="\n")
 		exit(1)
+
+	cophcorr = "-coph" in sys.argv
 
 	try:
 		sep = sys.argv[1 + sys.argv.index("-sep")]
@@ -217,7 +306,8 @@ if __name__ == "__main__":
 	ans = Hierclus.run(
 		dataset.iloc[:,:].values, 
 		criterion=sys.argv[2],
-		min_cluster_num=n)
+		min_cluster_num=n,
+		cophenetic_corr=cophcorr)
 
 	if ans is not None:
 		print("Dendrogram:")
