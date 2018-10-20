2a5ecfbdd148ec541b027cbf749702029da59563
FelSiq <felipe.siqueira@usp.br>
2018-10-20 16:07:08 -0300

    Graph generation for additional tests

--- testing-section/apriori-testing/apriori-test.py
+++ testing-section/apriori-testing/apriori-test.py
@@ -1,12 +1,12 @@
-import numpy
 from apyori import apriori
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 	import sys
 	if len(sys.argv) < 2:
 		print("usage: " + sys.argv[0] + " <data_filepath>",
-			"[-sep data_separator, default is \",\"",
-			"[-encoding data_encoding, default is \"utf-8\"",
+			"[-sep data_separator, default is \",\"]",
+			"[-encoding data_encoding, default is \"utf-8\"]",
 			"[-supp minimal_support_value, default is 0.1]",
 			"[-lift minimal_lift_value, default is 0]",
 			"[-conf minimal_confidence_value, default is 0.1]", sep="\n\t")
@@ -52,3 +52,61 @@ if __name__ == "__main__":
 	for res in results:
 		print("Items= [", ", ".join(res[0]), 
 			"]\n\tLift=", res[2][0][3], end="\n\n")
+
+	print("total rules generated:", len(results))
+
+	"""
+	x=[]
+	y=[]
+
+	for val in range(10, 251, 5):
+		x_val = val/1000
+		y_val = len(list(apriori(data,
+			min_support=x_val,
+			min_confidence=x_val,
+			min_lift=lift)))
+		x.append(x_val)
+		y.append(y_val)
+
+	plt.subplot(2, 2, 1)
+	plt.title("[0.010, 0.250]")
+	plt.plot(x, y)
+	plt.xlabel("Suporte/Confiança mínimos")
+	plt.ylabel("Regras aceitas")
+	plt.subplot(2, 2, 2)
+	plt.title("[0.015, 0.250]")
+	plt.plot(x[1:], y[1:])
+	plt.xlabel("Suporte/Confiança mínimos")
+	plt.ylabel("Regras aceitas")
+	plt.subplot(2, 2, 3)
+	plt.title("[0.020, 0.250]")
+	plt.plot(x[2:], y[2:])
+	plt.xlabel("Suporte/Confiança mínimos")
+	plt.ylabel("Regras aceitas")
+	plt.subplot(2, 2, 4)
+	plt.title("[0.025, 0.250]")
+	plt.xlabel("Suporte/Confiança mínimos")
+	plt.ylabel("Regras aceitas")
+	plt.plot(x[3:], y[3:])
+	plt.subplots_adjust(hspace=0.5, wspace=0.25)
+	plt.show()
+
+
+	x=[]
+	y=[]
+
+	for val in range(16):
+		x_val = val
+		y_val = len(list(apriori(data,
+			min_support=0.012,
+			min_confidence=0.012,
+			min_lift=val)))
+		x.append(x_val)
+		y.append(y_val)
+
+	plt.plot(x, y)
+	plt.title("Regras aceitas por Lift Mínimo")
+	plt.xlabel("Lift mínimo")
+	plt.ylabel("Regras aceitas")
+	plt.show()
+	"""
