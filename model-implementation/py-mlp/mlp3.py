41750d7db1b9809dbcb91ff416c5955a7bc0f758
FelSiq <felipe.siqueira@usp.br>
2019-06-02 20:48:48 -0300

    Updating new version of mlp

--- model-implementation/py-mlp/mlp3.py
+++ model-implementation/py-mlp/mlp3.py
@@ -67,24 +67,24 @@ class MLNetwork:
                           "".format(activation), UserWarning)
 
         self.act_fun, self.act_fun_deriv = self._ACTIVATION_FUNCS.get(
-            activation, self.f_tanh)
+            activation, (self.f_tanh, self.f_tanh_deriv))
 
     def _init_weights(self, learning_speed: float) -> None:
         """Init weights at random."""
         shape = np.hstack((
             self._num_attr,
             self.hidden_shape,
-            self._num_classes,
+            self._num_output,
         )).astype(np.int64)
 
-        self.weights = np.array([
+        self.weights = [
             np.random.normal(
                 loc=0.0,
                 scale=np.power(shape[i], -0.5),
                 size=(1 + shape[i]) * shape[i + 1],
             ).reshape((1 + shape[i], shape[i + 1]))
             for i in range(shape.size - 1)
-        ])
+        ]
 
         self.learning_speed = np.array([
             np.repeat(learning_speed, n)
@@ -114,16 +114,30 @@ class MLNetwork:
         if normalize:
             self.X /= self.X.std(axis=0)
 
-        self.y = sklearn.preprocessing.OneHotEncoder(
-            categories="auto", sparse=False
-        ).fit_transform(self.y.reshape(-1, 1))
+        if self.y.ndim == 1:
+            self._num_classes = np.unique(self.y).size
+            if self._num_classes > 2:
+                self.y = sklearn.preprocessing.OneHotEncoder(
+                    categories="auto", sparse=False
+                ).fit_transform(self.y.reshape(-1, 1))
+                self._num_output = self._num_classes
+
+            else:
+                self.y = self.y.reshape(-1, 1)
+                self._num_output = 1
+
+        else:
+            self._num_classes = self.y.shape[1]
+            self._num_output = self._num_classes
 
         self._num_inst = self.X.shape[0]
         self._num_attr = self.X.shape[1]
-        self._num_classes = self.y.shape[1]
 
         if shuffle:
-            np.random.shuffle(self.X)
+            indexes = np.arange(self._num_inst)
+            np.random.shuffle(indexes)
+            self.X = self.X[indexes, :]
+            self.y = self.y[indexes, :]
 
         self._init_weights(learning_speed)
 
@@ -133,7 +147,7 @@ class MLNetwork:
         """Feed the network with the current instance."""
         net = instance.copy()
 
-        num_layers = self.weights.shape[0] + 1
+        num_layers = len(self.weights) + 1
 
         self.layer_net = num_layers * [None]
         self.layer_output = num_layers * [None]
@@ -145,7 +159,7 @@ class MLNetwork:
 
             # Save layer outputs and net for backpropagation
             self.layer_net[layer_index] = net
-            self.layer_output[layer_index] = self.act_fun(net)
+            net = self.layer_output[layer_index] = self.act_fun(net)
 
         return net
 
@@ -162,23 +176,24 @@ class MLNetwork:
             np.vstack((self.layer_output[-2].reshape(-1, 1), 1.0)),
             (self.learning_speed[-1] * deltas).reshape(1, -1))
 
-        for i in np.arange(self.weights.shape[0]-1, 0, -1):
+        for i in np.arange(len(self.weights)-1, 0, -1):
             cur_weights = self.weights[i-1]
 
-            weight_adjust = np.zeros(cur_weights.shape)
-            cur_layer_neurons, prev_layer_neurons = cur_weights.shape
+            prev_layer_neurons, cur_layer_neurons = cur_weights.shape
 
+            weight_adjust = np.zeros(cur_weights.shape)
             new_deltas = np.zeros(cur_layer_neurons)
+
+            aux = np.hstack((self.layer_output[i-1], 1.0))
 
             for j in np.arange(cur_layer_neurons):
                 cur_learning_speed = self.learning_speed[i-1][j]
                 new_deltas[j] = (self.act_fun_deriv(self.layer_net[i][j]) *
-                                 np.dot(deltas, self.weights[i][j]))
+                                 np.dot(deltas, self.weights[i][j, :]))
 
                 for k in np.arange(prev_layer_neurons):
-                    weight_adjust[k, j] = (
-                        cur_learning_speed * new_deltas[j] *
-                        np.hstack((self.layer_output[i-1], 1.0))[k])
+                    weight_adjust[k, j] = (cur_learning_speed *
+                                           new_deltas[j] * aux[k])
 
             cur_weights -= weight_adjust
             deltas = new_deltas
@@ -194,10 +209,9 @@ class MLNetwork:
 
         it_num = 0
 
-        prev_diff = 1.0 + epsilon
-        batch_prev_error = batch_mean_error = 0.0
+        batch_mean_error = 1.0 + epsilon
 
-        while it_num < epochs and prev_diff > epsilon:
+        while it_num < epochs and batch_mean_error > epsilon:
             it_num += 1
 
             batch_indexes = np.random.choice(
@@ -205,7 +219,6 @@ class MLNetwork:
                 size=batch_size,
                 replace=False)
 
-            batch_prev_error = batch_mean_error
             batch_mean_error = 0.0
 
             # Stochastic gradient descent
@@ -222,10 +235,10 @@ class MLNetwork:
 
                 batch_mean_error += self.loss_function(output, cur_label)
 
-            batch_mean_error = batch_mean_error.mean() / batch_size
-            prev_diff = abs(batch_mean_error - batch_prev_error)
+            batch_mean_error = batch_mean_error.sum() / batch_size
 
-            print("Iteration id: {} - cur diff: {}".format(it_num, prev_diff))
+            print("Iteration id: {} - cur batch error: {}"
+                  "".format(it_num, batch_mean_error))
 
         return self
 
@@ -235,22 +248,31 @@ if __name__ == "__main__":
     from sklearn import model_selection
     iris = datasets.load_iris()
     X = iris.data
+    X = (X - X.mean(axis=0)) / X.std(axis=0)
     y = iris.target
 
-    sfk = model_selection.StratifiedKFold(n_splits=10)
+    n_splits = 10
+
+    sfk = model_selection.StratifiedKFold(n_splits=n_splits)
 
     acc = 0.0
     for train, test in sfk.split(X, y):
-        model = MLNetwork(5)
-        model.fit(X[train, :], y[train], learning_speed=0.001)
-        model.train(batch_size=train.size, epochs=200)
-
         labels = sklearn.preprocessing.OneHotEncoder(
             categories="auto", sparse=False
-        ).fit_transform(y[test].reshape(-1, 1))
+        ).fit_transform(y.reshape(-1, 1))
+
+        model = MLNetwork(4, activation="tanh")
+
+        model.fit(
+            X[train, :],
+            labels[train, :],
+            normalize=False,
+            learning_speed=0.01)
+
+        model.train(batch_size=train.size, epochs=400)
 
         cur_acc = 0.0
-        for inst, lab in zip(X[test, :], labels):
+        for inst, lab in zip(X[test, :], labels[test, :]):
             prediction = np.round(model.forward(inst))
             cur_acc += np.allclose(prediction, lab)
 
@@ -258,4 +280,24 @@ if __name__ == "__main__":
 
         acc += cur_acc
 
-    print("acc:", acc / 10)
+    print("acc:", acc / n_splits)
+
+    """
+    X = np.array([
+        [1, 1],
+        [1, 0],
+        [0, 0],
+        [0, 1],
+    ])
+
+    y = np.array([-1, 1, -1, 1])
+
+    model = MLNetwork(2, activation="tanh")
+    model.fit(X, y, learning_speed=0.05, shuffle=False, normalize=True)
+    model.train(batch_size=4, epochs=50000)
+
+    X = (X - X.mean(axis=0)) / X.std(axis=0)
+
+    for inst in X:
+        print(inst, model.forward(inst))
+    """
