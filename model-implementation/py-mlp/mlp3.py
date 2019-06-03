77b054c12f8f23a5f302f705cd71b839eff02ba6
FelSiq <felipe.siqueira@usp.br>
2019-06-02 23:50:08 -0300

    More experiments with mlp3

--- model-implementation/py-mlp/mlp3.py
+++ model-implementation/py-mlp/mlp3.py
@@ -10,7 +10,7 @@ class MLNetwork:
     """Implements a generic multilayer network."""
 
     @staticmethod
-    def f_tanh(x: np.ndarray, alpha: float = 0.0) -> np.ndarray:
+    def f_tanh(x: np.ndarray, alpha: float = 0.005) -> np.ndarray:
         """Hyperbolic tangent function recommended in reference paper.
 
         f(x) = 1.7159 * tanh(2/3 * x) + alpha * x
@@ -24,7 +24,7 @@ class MLNetwork:
         return 1.7159 * np.tanh(0.6667 * x) + alpha * x
 
     @staticmethod
-    def f_tanh_deriv(x: np.ndarray, alpha: float = 0.0) -> np.ndarray:
+    def f_tanh_deriv(x: np.ndarray, alpha: float = 0.005) -> np.ndarray:
         """Derivative of the recommended activation function."""
         return 1.1440 * np.power(np.cosh(0.6667 * x), -2.0) + alpha
 
@@ -63,8 +63,9 @@ class MLNetwork:
         }
 
         if activation not in self._ACTIVATION_FUNCS:
-            warnings.warn("Given 'activation' is unknown ({})"
-                          "".format(activation), UserWarning)
+            warnings.warn(
+                "Given 'activation' is unknown ({})"
+                "".format(activation), UserWarning)
 
         self.act_fun, self.act_fun_deriv = self._ACTIVATION_FUNCS.get(
             activation, (self.f_tanh, self.f_tanh_deriv))
@@ -86,10 +87,21 @@ class MLNetwork:
             for i in range(shape.size - 1)
         ]
 
-        self.learning_speed = np.array([
-            np.repeat(learning_speed, n)
-            for n in shape[1:]
-        ])
+        self.averaged_gradient = [
+            np.repeat(
+                learning_speed,
+                (1 + shape[i]) * shape[i + 1]
+            ).reshape((1 + shape[i], shape[i + 1]))
+            for i in range(shape.size - 1)
+        ]
+
+        self.learning_speed = [
+            np.repeat(
+                np.sqrt(shape[i]) * learning_speed,
+                (1 + shape[i]) * shape[i + 1]
+            ).reshape((1 + shape[i], shape[i + 1]))
+            for i in range(shape.size - 1)
+        ]
 
     def fit(self,
             X: np.ndarray,
@@ -97,6 +109,7 @@ class MLNetwork:
             learning_speed: float,
             normalize: bool = True,
             shuffle: bool = True,
+            apply_pca: bool = False,
             random_seed: t.Optional[int] = None) -> "MLNetwork":
         """Fit data into model."""
 
@@ -108,8 +121,9 @@ class MLNetwork:
         if normalize:
             self.X = self.X - self.X.mean(axis=0)
 
-        self.X = sklearn.decomposition.PCA(
-            copy=False, random_state=random_seed).fit_transform(self.X)
+        if apply_pca:
+            self.X = sklearn.decomposition.PCA(
+                copy=False, random_state=random_seed).fit_transform(self.X)
 
         if normalize:
             self.X /= self.X.std(axis=0)
@@ -118,8 +132,8 @@ class MLNetwork:
             self._num_classes = np.unique(self.y).size
             if self._num_classes > 2:
                 self.y = sklearn.preprocessing.OneHotEncoder(
-                    categories="auto", sparse=False
-                ).fit_transform(self.y.reshape(-1, 1))
+                    categories="auto", sparse=False).fit_transform(
+                        self.y.reshape(-1, 1))
                 self._num_output = self._num_classes
 
             else:
@@ -163,61 +177,89 @@ class MLNetwork:
 
         return net
 
-    def _backward(self,
-                  output: np.ndarray,
-                  cur_inst: np.ndarray,
-                  cur_label: np.ndarray) -> None:
+    def _backward(self, output: np.ndarray, cur_inst: np.ndarray,
+                  cur_label: np.ndarray, momentum: float = 0.0
+                  ) -> t.Sequence[np.ndarray]:
         """Apply backpropagation and adjust network weights."""
-        deltas = (self.act_fun_deriv(output) *
-                  self.loss_function_deriv(cur_label, output))
+        deltas = (self.act_fun_deriv(output) * self.loss_function_deriv(
+            cur_label, output))
 
-        # Adjust output layer later
-        output_adjust = np.dot(
-            np.vstack((self.layer_output[-2].reshape(-1, 1), 1.0)),
-            (self.learning_speed[-1] * deltas).reshape(1, -1))
+        adjust_weights = [
+            np.multiply(
+                np.dot(
+                    np.vstack((self.layer_output[-2].reshape(-1, 1), 1.0)),
+                    deltas.reshape(1, -1)),
+                self.learning_speed[-1]
+            )
+        ]
 
-        for i in np.arange(len(self.weights)-1, 0, -1):
-            cur_weights = self.weights[i-1]
+        for i in np.arange(len(self.weights) - 1, 0, -1):
+            cur_weights = self.weights[i - 1]
 
             prev_layer_neurons, cur_layer_neurons = cur_weights.shape
 
-            weight_adjust = np.zeros(cur_weights.shape)
+            cur_weight_adjust = np.zeros(cur_weights.shape)
             new_deltas = np.zeros(cur_layer_neurons)
 
-            aux = np.hstack((self.layer_output[i-1], 1.0))
+            bias_out = np.hstack((self.layer_output[i - 1], 1.0))
 
             for j in np.arange(cur_layer_neurons):
-                cur_learning_speed = self.learning_speed[i-1][j]
                 new_deltas[j] = (self.act_fun_deriv(self.layer_net[i][j]) *
                                  np.dot(deltas, self.weights[i][j, :]))
 
                 for k in np.arange(prev_layer_neurons):
-                    weight_adjust[k, j] = (cur_learning_speed *
-                                           new_deltas[j] * aux[k])
+                    cur_weight_adjust[k, j] = (
+                        self.learning_speed[i - 1][k, j] *
+                        new_deltas[j] * bias_out[k])
 
-            cur_weights -= weight_adjust
+            adjust_weights.insert(0, cur_weight_adjust)
             deltas = new_deltas
 
-        # Adjust the output layer
-        self.weights[-1] -= output_adjust
+        for i in np.arange(len(self.weights)):
+            self.weights[i] -= adjust_weights[i] + momentum * self.weights[i]
+
+        return adjust_weights
+
+    def _update_learning_speed(self,
+                               alpha: float,
+                               beta: float,
+                               gamma: float,
+                               adjust_weights: t.Sequence[np.ndarray]) -> None:
+        """Update the learning speed of each weight."""
+        for i in np.arange(len(self.averaged_gradient)):
+            self.averaged_gradient[i] = (
+                (1.0 - gamma)
+                * self.averaged_gradient[i]
+                + gamma * adjust_weights[i])
+
+            self.learning_speed[i] = (
+                self.learning_speed[i] + self.learning_speed[i] * alpha *
+                (beta * np.linalg.norm(self.averaged_gradient[i], ord=2)
+                 - self.learning_speed[i])
+            )
 
     def train(self,
               batch_size: int = 1,
               epochs: int = 50,
-              epsilon: float = 1.0e-8) -> "MLNetwork":
+              epsilon: float = 1.0e-8,
+              update_learning_rate: bool = False,
+              alpha: float = 0.0,
+              beta: float = 0.0,
+              gamma: float = 0.0,
+              momentum: float = 0.0) -> "MLNetwork":
         """Train the network weights with fitted data."""
 
         it_num = 0
 
         batch_mean_error = 1.0 + epsilon
 
+        it_to_print = epochs // 10
+
         while it_num < epochs and batch_mean_error > epsilon:
             it_num += 1
 
             batch_indexes = np.random.choice(
-                a=self._num_inst,
-                size=batch_size,
-                replace=False)
+                a=self._num_inst, size=batch_size, replace=False)
 
             batch_mean_error = 0.0
 
@@ -228,22 +270,32 @@ class MLNetwork:
 
                 output = self.forward(cur_inst)
 
-                self._backward(
+                adjust_weights = self._backward(
                     output=output,
                     cur_inst=cur_inst,
-                    cur_label=cur_label)
+                    cur_label=cur_label,
+                    momentum=momentum)
+
+                if update_learning_rate:
+                    self._update_learning_speed(
+                        alpha=alpha,
+                        beta=beta,
+                        gamma=gamma,
+                        adjust_weights=adjust_weights)
 
                 batch_mean_error += self.loss_function(output, cur_label)
 
             batch_mean_error = batch_mean_error.sum() / batch_size
 
-            print("Iteration id: {} - cur batch error: {}"
-                  "".format(it_num, batch_mean_error))
+            if it_num % it_to_print == 0:
+                print("Iteration id: {} - cur batch error: {}"
+                      "".format(it_num, batch_mean_error))
 
         return self
 
 
 if __name__ == "__main__":
+    """
     from sklearn import datasets
     from sklearn import model_selection
     iris = datasets.load_iris()
@@ -251,15 +303,14 @@ if __name__ == "__main__":
     X = (X - X.mean(axis=0)) / X.std(axis=0)
     y = iris.target
 
-    n_splits = 10
+    n_splits = 5
 
     sfk = model_selection.StratifiedKFold(n_splits=n_splits)
 
     acc = 0.0
     for train, test in sfk.split(X, y):
         labels = sklearn.preprocessing.OneHotEncoder(
-            categories="auto", sparse=False
-        ).fit_transform(y.reshape(-1, 1))
+            categories="auto", sparse=False).fit_transform(y.reshape(-1, 1))
 
         model = MLNetwork(4, activation="tanh")
 
@@ -269,7 +320,7 @@ if __name__ == "__main__":
             normalize=False,
             learning_speed=0.01)
 
-        model.train(batch_size=train.size, epochs=400)
+        model.train(batch_size=train.size, epochs=1000)
 
         cur_acc = 0.0
         for inst, lab in zip(X[test, :], labels[test, :]):
@@ -281,7 +332,6 @@ if __name__ == "__main__":
         acc += cur_acc
 
     print("acc:", acc / n_splits)
-
     """
     X = np.array([
         [1, 1],
@@ -293,11 +343,9 @@ if __name__ == "__main__":
     y = np.array([-1, 1, -1, 1])
 
     model = MLNetwork(2, activation="tanh")
-    model.fit(X, y, learning_speed=0.05, shuffle=False, normalize=True)
+    model.fit(X, y, learning_speed=0.01, normalize=True)
     model.train(batch_size=4, epochs=50000)
 
     X = (X - X.mean(axis=0)) / X.std(axis=0)
-
     for inst in X:
         print(inst, model.forward(inst))
-    """
