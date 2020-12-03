845b8b3b3ff4a41cac2255c60cb0f7a9b54f8949
felsiq <felipe.siqueira@usp.br>
2020-12-03 15:13:26 -0300

    Add Adam optimizer

--- model-implementation/py-deep-network/optimizers.py
+++ model-implementation/py-deep-network/optimizers.py
@@ -45,7 +45,7 @@ def init_momentum(
 
     cache["beta1"] = beta1
     cache["bias_correction"] = bias_correction
-    cache["it_num"] = 0
+    cache["momentum_it_num"] = 0
 
     issue_unused_args_warnings(kwargs)
 
@@ -57,7 +57,7 @@ def update_momentum(
 ) -> t.Dict[str, np.ndarray]:
     bias_correction = cache["bias_correction"]
     beta1 = cache["beta1"]
-    it_num = cache["it_num"]
+    it_num = cache["momentum_it_num"]
 
     updates = dict()
 
@@ -75,7 +75,7 @@ def update_momentum(
             cur_vel /= 1.0 + beta1 ** it_num
 
         cache[name_v_param] = cur_vel
-        cache["it_num"] += 1
+        cache["momentum_it_num"] += 1
         updates[name_u_param] = cur_vel
 
     return updates
@@ -85,6 +85,7 @@ def init_rmsprop(
     params: t.Dict[str, np.ndarray],
     beta2: float = 0.999,
     epsilon: float = 1e-8,
+    bias_correction: bool = True,
     **kwargs,
 ) -> t.Dict[str, np.ndarray]:
     cache = dict()
@@ -97,6 +98,8 @@ def init_rmsprop(
 
     cache["beta2"] = beta2
     cache["epsilon"] = epsilon
+    cache["bias_correction"] = bias_correction
+    cache["rmsprop_it_num"] = 0
 
     issue_unused_args_warnings(kwargs)
 
@@ -109,6 +112,8 @@ def update_rmsprop(
 
     beta2 = cache["beta2"]
     epsilon = cache["epsilon"]
+    bias_correction = cache["bias_correction"]
+    it_num = cache["rmsprop_it_num"]
 
     updates = dict()
 
@@ -122,8 +127,59 @@ def update_rmsprop(
         cur_vel_sqr = cache[name_s_param]
         cur_vel_sqr = beta2 * cur_vel_sqr + (1.0 - beta2) * np.square(cur_grad)
 
+        if bias_correction and beta2 ** it_num > 1e-8:
+            cur_vel_sqr /= 1.0 + beta2 ** it_num
+
         cache[name_s_param] = cur_vel_sqr
         updates[name_u_param] = cur_grad / (np.sqrt(cur_vel_sqr) + epsilon)
+
+    return updates
+
+
+def init_adam(
+    params: t.Dict[str, np.ndarray],
+    beta1: float = 0.9,
+    beta2: float = 0.999,
+    epsilon: float = 1e-8,
+    bias_correction: bool = True,
+    **kwargs,
+) -> t.Dict[str, np.ndarray]:
+    cache = dict()
+
+    cache.update(init_momentum(params, beta1=beta1, bias_correction=bias_correction))
+    cache.update(
+        init_rmsprop(
+            params, beta2=beta2, epsilon=epsilon, bias_correction=bias_correction
+        )
+    )
+
+    issue_unused_args_warnings(kwargs)
+
+    return cache
+
+
+def update_adam(
+    grads: t.Dict[str, np.ndarray], cache: t.Dict[str, t.Any]
+) -> t.Dict[str, np.ndarray]:
+    updates = dict()
+
+    epsilon = cache["epsilon"]
+
+    # Note: both return values below are unused, calling just to update caches.
+    update_momentum(grads, cache)
+    update_rmsprop(grads, cache)
+
+    for dparam, cur_grad in grads.items():
+        name_s_param = "s" + dparam[1:]
+        name_v_param = "v" + dparam[1:]
+        name_u_param = "u" + dparam[1:]
+
+        if not (name_s_param in cache and name_v_param in cache):
+            continue
+
+        updates[name_u_param] = cache[name_v_param] / (
+            np.sqrt(cache[name_s_param]) + epsilon
+        )
 
     return updates
 
@@ -132,4 +188,5 @@ OPTIM = {
     "gd": (init_gd, update_gd),
     "momentum": (init_momentum, update_momentum),
     "rmsprop": (init_rmsprop, update_rmsprop),
+    "adam": (init_adam, update_adam),
 }  # type: t.Dict[str, t.Tuple[t.Callable, t.Callable]]
