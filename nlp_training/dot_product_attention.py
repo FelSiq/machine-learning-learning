9defbfe3339746478c70a4d04efe939aa767621f
felsiq <felipe.siqueira@usp.br>
2020-12-26 11:48:27 -0300

    Add causal/self attention

--- nlp_training/dot_product_attention.py
+++ nlp_training/dot_product_attention.py
@@ -1,6 +1,11 @@
 """Dot-product attention to perform token alignment.
 
 Often used with Transformers.
+
+There are three common types of attention mechanism:
+    - Encoder-Decoder attention (build attention comparing two distinct sequences)
+    - Self attention/causal attention (build attention from a single sequence but only previous tokens)
+    - Bidirectional attention (build attention from a single sequence and all tokens)
 """
 import numpy as np
 
@@ -16,6 +21,19 @@ def dp_attention(queries, keys, values):
     return attention_score
 
 
+def causal_attention(queries, keys, values):
+    """Also known as 'self attention'.
+
+    A token's attention score can only be calculated from the
+    previous tokens of the same sequence.
+    """
+    mask_shape = (queries.shape[0], keys.shape[0])
+    mask = np.triu(np.full(mask_shape, fill_value=-np.inf), k=1)
+    attention_weights = _softmax(np.dot(queries, keys.T) + mask)
+    attention_score = np.dot(attention_weights, values)
+    return attention_score
+
+
 def _test():
     q_dim = 5
     v_dim = 7
@@ -25,6 +43,8 @@ def _test():
     V = np.random.randn(v_dim, d_model)
 
     att = dp_attention(Q, V, V)
+    att = causal_attention(Q, V, V)
+    print(att)
 
 
 if __name__ == "__main__":
