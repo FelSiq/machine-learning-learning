e37dfb6db4ea7c2c4b6a6432687181f5b1f9528d
felsiq <felipe.siqueira@usp.br>
2020-12-29 21:06:27 -0300

    Improved model training code in general

--- nlp_training/named_entity_recog.py
+++ nlp_training/named_entity_recog.py
@@ -93,14 +93,40 @@ def masked_cross_entropy(y_preds, y_batch, y_len, device):
     return loss
 
 
+def calc_baseline_acc(y_train):
+    freq = collections.Counter()
+
+    for tags in y_train:
+        freq.update(tags.cpu().numpy())
+
+    _, most_common_class_freq = freq.most_common(1)[0]
+
+    return most_common_class_freq / sum(freq.values())
+
+
+def eval_model(model, X_eval, X_eval_lens, y_eval, baseline_acc):
+    print("\nEvaluating...")
+    model.eval()
+    y_preds = model(X_eval, X_eval_lens).argmax(axis=2).detach().cpu()
+    eval_acc = 0.0
+
+    for k, y_cur in enumerate(y_eval):
+        preds_cur = y_preds[k, : len(y_cur)]
+        eval_acc += sum(y_cur == preds_cur).item() / len(y_cur)
+
+    eval_acc /= len(y_eval)
+    print(f"Evaluation acc: {eval_acc:.4f} (baseline: {baseline_acc:.4f})")
+
+
 def _test():
     min_freq_vocab = 2
     hold_out_validation_frac = 0.025
-    train_epochs = 2
+    train_epochs = 0
     batch_size = 512
     embed_dim = 128
     num_lstm = 1
     device = "cuda"
+    checkpoint_path = "ner_torch_model.pt"
 
     X, y, vocab, tags = ner_utils.get_data(min_freq_vocab)
 
@@ -132,12 +158,22 @@ def _test():
     print("Train size:", train_size)
     print("Eval size :", len(X) - train_size)
 
+    maj_class_acc = calc_baseline_acc(y_train)
+    print(f"Baseline accuracy: {maj_class_acc:.4f}")
+
     model = RNNNER(
         vocab_size=len(vocab),
         embed_dim=embed_dim,
         num_tags=len(tags),
         num_lstm=num_lstm,
     ).to(device)
+
+    try:
+        model.load_state_dict(torch.load(checkpoint_path))
+        print("Checkpoint model loaded.")
+
+    except FileNotFoundError:
+        pass
 
     optim = torch.optim.Adam(model.parameters(), lr=0.01)
 
@@ -159,17 +195,13 @@ def _test():
             loss.backward()
             optim.step()
 
-        print("\nEvaluating...")
-        model.eval()
-        y_preds = model(X_eval, X_eval_lens).argmax(axis=2).detach().cpu()
-        eval_acc = 0.0
+        if epoch < train_epochs - 1:
+            eval_model(model, X_eval, X_eval_lens, y_eval, maj_class_acc)
 
-        for k, y_cur in enumerate(y_eval):
-            preds_cur = y_preds[k, : len(y_cur)]
-            eval_acc += sum(y_cur == preds_cur).item() / len(y_cur)
+    print("Finished training.")
+    eval_model(model, X_eval, X_eval_lens, y_eval, maj_class_acc)
 
-        eval_acc /= len(y_eval)
-        print(f"Evaluation acc: {eval_acc:.4f}")
+    torch.save(model.state_dict(), checkpoint_path)
 
     print("Done.")
 
