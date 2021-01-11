bd791066bf734bf90f0750e6eb3688efba1d8871
felsiq <felipe.siqueira@usp.br>
2021-01-11 19:14:38 -0300

    Updated get_data.sh

--- nlp-training/machine_translation/get_data.sh
+++ nlp-training/machine_translation/get_data.sh
@@ -1,16 +1,35 @@
 # Data collected from: https://paracrawl.eu/
-echo 'Creating corpus directory...'
-mkdir ./corpus
+echo 'Grab a cup of coffee. This will take a while to complete.'
+
+echo '(1 / 4) Creating corpus directory...'
+if [ ! -d "./corpus" ] then
+	mkdir ./corpus
+else
+	echo "Output directory found, skipping this step."
+fi
 echo 'Done.'
 
-echo 'Collecting data...'
-wget https://s3.amazonaws.com/web-language-models/paracrawl/release7.1/en-fi.txt.gz -O ./corpus/en-fi.txt.gz
+echo '(2 / 4) Collecting data...'
+GZ_FILE = ./corpus/en-fi.txt.gz
+
+if [ -f "$GZ_FILE" || -f "corpus/en-fi.txt"] ; then
+	echo ".gz or extracted file found, skipping this step."
+else
+	wget https://s3.amazonaws.com/web-language-models/paracrawl/release7.1/en-fi.txt.gz -O "$GZ_FILE"
+fi
+
 echo 'Done.'
 
-echo 'Extracting data...'
-gzip -d corpus/en-fi.txt.gz
+echo '(3 / 4) Extracting data...'
+
+if [ -f "corpus/en-fi.txt" ] then
+	echo "Extracted file found, skipping this step."
+else
+	gzip -d corpus/en-fi.txt.gz
+fi
+
 echo 'Done.'
 
-echo 'Building Byte Pair Encoding vocabulary...'
+echo '(4 / 4) Building Byte Pair Encoding vocabulary...'
 python prepare_data.py
 echo 'Done.'
