c4a6f2ee6adba7c83d43b4aa8f347bdcfb70cf99
felsiq <felipe.siqueira@usp.br>
2021-01-11 18:38:41 -0300

    Spliting full data into train/eval subsets

--- nlp-training/machine_translation/prepare_data.py
+++ nlp-training/machine_translation/prepare_data.py
@@ -1,11 +1,31 @@
+import os
+
 import sentencepiece
 import pandas as pd
 
 import utils
 
 
-if __name__ == "__main__":
-    datagen = utils.load_data_from_file(int(4e6), None)
+def split_train_eval_sets(train_frac: float = 0.99):
+    data = utils.load_data_from_file("corpus/en-fi.txt", None, None)
+
+    train_size = int(train_frac * len(data))
+
+    print("Train fraction :", train_frac)
+    print("Train size     :", train_size)
+    print("Eval size      :", len(data) - train_size)
+
+    data.iloc[:train_size, :].to_csv(
+        "corpus/en-fi-train.txt", index=False, header=False
+    )
+    data.iloc[train_size:, :].to_csv("corpus/en-fi-eval.txt", index=False, header=False)
+
+    del data
+    del train_size
+
+
+def create_byte_pair_encoding_vocab(max_sentences: int = int(4e6)):
+    datagen = utils.load_data_from_file("corpus/en-fi-train.txt", max_sentences, None)
 
     datagen["en"].to_csv("corpus/en-only.txt", index=False, header=False)
     datagen["fi"].to_csv("corpus/fi-only.txt", index=False, header=False)
@@ -27,3 +47,19 @@ if __name__ == "__main__":
         shuffle_input_sentence=True,
         pad_id=3,
     )
+
+
+if __name__ == "__main__":
+    if os.path.isfile("corpus/en-fi-train.txt") and os.path.isfile(
+        "corpus/en-fi-eval.txt"
+    ):
+        print("Train/eval files found. Skipping data split.")
+
+    else:
+        print("Splitting data into train and evaluation files...")
+        split_train_eval_sets()
+        print("Done.")
+
+    print("Creating Byte Pair Encoding vocabulary using train files...")
+    create_byte_pair_encoding_vocab()
+    print("Done.")
