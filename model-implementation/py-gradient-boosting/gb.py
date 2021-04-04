4cfbf63b8255e572893fe4b6dcebdc047a6e2973
felsiq <felipe.siqueira@usp.br>
2021-04-04 15:35:32 -0300

    Working version of multiclass classification with gb

--- model-implementation/py-gradient-boosting/gb.py
+++ model-implementation/py-gradient-boosting/gb.py
@@ -26,7 +26,7 @@ class _GradientBoostingBase(sklearn.base.BaseEstimator):
         self.learning_rate = float(learning_rate)
 
         self.n_estimators = int(n_estimators)
-        self.estimators = []  # type: t.List[_LearnerType]
+        self.estimator_layers = []  # type: t.List[_LearnerType]
 
         self._estimator_gen = functools.partial(
             sklearn.tree.DecisionTreeRegressor,
@@ -71,42 +71,56 @@ class _GradientBoostingBase(sklearn.base.BaseEstimator):
         self._prepare_fit()
         self._n_labels = y.shape[1]
 
-        self.estimators = []
+        self.estimator_layers = []
         self._baseline = self._calc_baseline(y)
-        cur_preds = np.tile(self._baseline, y.shape)
+        cur_preds = np.tile(self._baseline, (y.shape[0], 1))
+
+        if isinstance(self, GradientBoostingClassifier):
+            cur_preds_probs = self._log_odds_to_prob(cur_preds)
+
+        else:
+            cur_preds_probs = np.zeros((1, self._n_labels))
 
         ref = self._calc_ref(y)
         pseudo_resid = ref - self._baseline
 
         for _ in tqdm.auto.tqdm(np.arange(self.n_estimators)):
-            estimator = self._estimator_gen()
-            estimator.fit(X, pseudo_resid)
+            layer = []
 
-            # Units: the same as 'y'
-            preds_pseudo_resid = estimator.predict(X)
+            for j in np.arange(self._n_labels):
+                estimator = self._estimator_gen()
+                estimator.fit(X, pseudo_resid[:, j])
 
-            # Units: the same as 'self._baseline' or 'cur_preds
-            preds = self._cast_pseudo_resid_to_preds(
-                X, estimator, preds_pseudo_resid, cur_preds[:, 0]
-            )
+                # Units: the same as 'y'
+                preds_pseudo_resid = estimator.predict(X)
 
-            cur_preds += self.learning_rate * preds
+                # Units: the same as 'self._baseline' or 'cur_preds
+                preds = self._cast_pseudo_resid_to_preds(
+                    X, estimator, preds_pseudo_resid, cur_preds_probs[:, j]
+                )
+
+                cur_preds[:, j] += self.learning_rate * preds
+                layer.append(estimator)
+
             pseudo_resid = ref - cur_preds
+            self.estimator_layers.append(layer)
 
-            self.estimators.append(estimator)
+            if isinstance(self, GradientBoostingClassifier):
+                cur_preds_probs = self._log_odds_to_prob(cur_preds)
 
         return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         X = self._check_X_y(X)
-        preds = np.full((X.shape[0], self._n_labels), fill_value=self._baseline)
+        preds = np.tile(self._baseline, (X.shape[0], 1))
 
-        for estimator in self.estimators:
-            preds_pseudo_resid = estimator.predict(X)
-            cur_preds = self._cast_pseudo_resid_to_preds(
-                X, estimator, preds_pseudo_resid, train=False
-            )
-            preds += self.learning_rate * cur_preds
+        for estimator_layer in self.estimator_layers:
+            for j, estimator in enumerate(estimator_layer):
+                preds_pseudo_resid = estimator.predict(X)
+                cur_preds = self._cast_pseudo_resid_to_preds(
+                    X, estimator, preds_pseudo_resid, train=False
+                )
+                preds[:, j] += self.learning_rate * cur_preds
 
         return np.squeeze(self._prepare_output(preds))
 
@@ -121,7 +135,7 @@ class _GradientBoostingBase(sklearn.base.BaseEstimator):
         X: np.ndarray,
         estimator: _LearnerType,
         pseudo_resids: t.Optional[np.ndarray] = None,
-        cur_preds: t.Optional[np.ndarray] = None,
+        cur_preds_probs: t.Optional[np.ndarray] = None,
         train: bool = True,
     ) -> np.ndarray:
         raise NotImplementedError
@@ -146,15 +160,10 @@ class GradientBoostingRegressor(_GradientBoostingBase):
         X: np.ndarray,
         estimator: _LearnerType,
         pseudo_resids: t.Optional[np.ndarray] = None,
-        cur_preds: t.Optional[np.ndarray] = None,
+        cur_preds_probs: t.Optional[np.ndarray] = None,
         train: bool = True,
     ) -> np.ndarray:
-        pseudo_resid = np.asfarray(pseudo_resids)
-
-        if pseudo_resid.ndim == 1:
-            pseudo_resid = np.expand_dims(pseudo_resid, axis=1)
-
-        return pseudo_resid
+        return np.asfarray(pseudo_resids)
 
     @classmethod
     def _calc_baseline(cls, y: np.ndarray) -> float:
@@ -177,17 +186,17 @@ class GradientBoostingClassifier(_GradientBoostingBase):
         X: np.ndarray,
         estimator: _LearnerType,
         pseudo_resids: t.Optional[np.ndarray] = None,
-        cur_preds: t.Optional[np.ndarray] = None,
+        cur_preds_probs: t.Optional[np.ndarray] = None,
         train: bool = True,
     ) -> np.ndarray:
         leaf_ids = estimator.apply(X)
 
         if train:
-            assert pseudo_resids is not None and cur_preds is not None
+            assert pseudo_resids is not None and cur_preds_probs is not None
             _pseudo_resids = np.asfarray(pseudo_resids)
-            _cur_preds = np.asfarray(cur_preds)
+            _cur_preds_probs = np.asfarray(cur_preds_probs)
             self._register_estimator_log_odds_by_leaf(
-                leaf_ids, estimator, _pseudo_resids, _cur_preds
+                leaf_ids, estimator, _pseudo_resids, _cur_preds_probs
             )
 
         _id_est = id(estimator)
@@ -197,9 +206,6 @@ class GradientBoostingClassifier(_GradientBoostingBase):
         region_ids = np.argmax(leaf_ids == all_leaf_ids, axis=0)
         pred_log_odds = log_odds_by_leaf[region_ids]
 
-        if pred_log_odds.ndim == 1:
-            pred_log_odds = np.expand_dims(pred_log_odds, axis=1)
-
         return pred_log_odds
 
     def _register_estimator_log_odds_by_leaf(
@@ -207,7 +213,7 @@ class GradientBoostingClassifier(_GradientBoostingBase):
         leaf_ids: np.ndarray,
         estimator: _LearnerType,
         pseudo_resids: np.ndarray,
-        cur_preds: np.ndarray,
+        cur_preds_probs: np.ndarray,
     ) -> None:
         _id_est = id(estimator)
 
@@ -216,7 +222,6 @@ class GradientBoostingClassifier(_GradientBoostingBase):
         all_leaf_ids, region_ids = np.unique(leaf_ids, return_inverse=True)
 
         total_var_by_leaf, total_resid_by_leaf = np.zeros((2, estimator.get_n_leaves()))
-        cur_preds_probs = self._sigmoid(cur_preds)
 
         np.add.at(total_resid_by_leaf, region_ids, pseudo_resids)
         np.add.at(
@@ -228,14 +233,20 @@ class GradientBoostingClassifier(_GradientBoostingBase):
         self._estimators_log_odds_by_leaf[_id_est] = log_odds_by_leaf
         self._estimators_leaf_id_translator[_id_est] = all_leaf_ids[:, np.newaxis]
 
-    def _prepare_output(
-        self,
-        log_odds: np.ndarray,
-    ):
+    def _log_odds_to_prob(self, log_odds: np.ndarray) -> t.Union[float, np.ndarray]:
+        # NOTE: 'log_odds' is multiplied by -1 to simulate the transformation
+        # from the cross entropy loss (which requires a gradient descent to minimize)
+        # to the maximum likelihood estimation of a multinomial distribution (which uses
+        # gradient ascent and, therefore, is complatible with our implementation of both
+        # regression and binary classification). I don't know if there is a more both
+        # appropriate and generic way to make this transformation in this context.
         probs = (
-            self._softmax(log_odds) if self._n_labels > 1 else self._sigmoid(log_odds)
+            self._softmax(-log_odds) if self._n_labels > 1 else self._sigmoid(log_odds)
         )
         return probs
+
+    def _prepare_output(self, log_odds: np.ndarray):
+        return self._log_odds_to_prob(log_odds)
 
     @staticmethod
     def _sigmoid(log_odds: t.Union[float, np.ndarray]) -> t.Union[float, np.ndarray]:
@@ -251,10 +262,10 @@ class GradientBoostingClassifier(_GradientBoostingBase):
         return np.asfarray(inst_log_odds)
 
     @classmethod
-    def _calc_baseline(cls, y: np.ndarray) -> float:
-        cls_prob = np.mean(y)
+    def _calc_baseline(cls, y: np.ndarray) -> t.Union[float, np.ndarray]:
+        cls_prob = np.mean(y, axis=0)
         cls_odds = cls_prob / (1.0 - cls_prob)
-        cls_log_odds = float(np.log(cls_odds))
+        cls_log_odds = np.log(cls_odds)
         return cls_log_odds
 
 
@@ -264,7 +275,8 @@ def _test():
     import sklearn.model_selection
     import sklearn.metrics
 
-    regression = True
+    regression = False
+    binary_class = True
 
     if regression:
         gb = GradientBoostingRegressor()
@@ -273,32 +285,49 @@ def _test():
     else:
         gb = GradientBoostingClassifier()
         sk = sklearn.ensemble.GradientBoostingClassifier()
-        X, y = sklearn.datasets.load_breast_cancer(return_X_y=True)
 
-    X_train, X_eval, y_train, y_eval = sklearn.model_selection.train_test_split(
-        X, y, test_size=0.2
-    )
+        if binary_class:
+            X, y = sklearn.datasets.load_breast_cancer(return_X_y=True)
+        else:
 
-    gb.fit(X_train, y_train)
-    sk.fit(X_train, y_train)
+            X, y = sklearn.datasets.load_iris(return_X_y=True)
 
-    gb_y_pred = gb.predict(X_eval)
-    sk_y_pred = sk.predict(X_eval)
+    splitter = sklearn.model_selection.KFold(n_splits=10, shuffle=True)
 
-    if regression:
-        gb_rmse = sklearn.metrics.mean_squared_error(gb_y_pred, y_eval, squared=False)
-        sk_rmse = sklearn.metrics.mean_squared_error(sk_y_pred, y_eval, squared=False)
+    gb_perf = sk_perf = 0.0
 
-        print(f"My rmse: {gb_rmse:.4f}")
-        print(f"Sk rmse: {sk_rmse:.4f}")
+    for train_inds, eval_inds in splitter.split(X, y):
+        X_train, X_eval = X[train_inds, :], X[eval_inds, :]
+        y_train, y_eval = y[train_inds], y[eval_inds]
 
-    else:
-        gb_y_pred = (gb_y_pred > 0.5).astype(int)
-        gb_acc = sklearn.metrics.accuracy_score(gb_y_pred, y_eval)
-        sk_acc = sklearn.metrics.accuracy_score(sk_y_pred, y_eval)
+        gb.fit(X_train, y_train)
+        sk.fit(X_train, y_train)
 
-        print(f"My acc: {gb_acc:.4f}")
-        print(f"Sk acc: {sk_acc:.4f}")
+        gb_y_pred = gb.predict(X_eval)
+        sk_y_pred = sk.predict(X_eval)
+
+        if regression:
+            gb_perf += sklearn.metrics.mean_squared_error(
+                gb_y_pred, y_eval, squared=False
+            )
+            sk_perf += sklearn.metrics.mean_squared_error(
+                sk_y_pred, y_eval, squared=False
+            )
+
+        else:
+            if gb_y_pred.ndim == 1:
+                gb_y_pred = gb_y_pred > 0.5
+
+            else:
+                gb_y_pred = gb_y_pred.argmax(axis=1)
+
+            gb_y_pred = gb_y_pred.astype(int, copy=False)
+
+            gb_perf += sklearn.metrics.accuracy_score(gb_y_pred, y_eval)
+            sk_perf += sklearn.metrics.accuracy_score(sk_y_pred, y_eval)
+
+    print(f"My perf: {gb_perf / 10:.4f}")
+    print(f"Sk perf: {sk_perf / 10:.4f}")
 
 
 if __name__ == "__main__":
