55cda716c938e33fc90b3224ac4f02054ae8a8a5
felsiq <felipe.siqueira@usp.br>
2021-04-29 13:16:50 -0300

    Add a new test with plots for GDA (LDA and QDA)

--- algorithm-implementation/py-discriminant-analysis/gaussian-discriminant-analysis.py
+++ algorithm-implementation/py-discriminant-analysis/gaussian-discriminant-analysis.py
@@ -24,8 +24,12 @@ import numpy as np
 import scipy.stats
 
 
-class GaussianDiscriminantAnalysis:
-    """Simple Gaussian Discriminant Analysis Classifier model."""
+class GDA:
+    """Simple Gaussian Discriminant Analysis Classifier model.
+
+    This algorithm chooses between Linear Discriminant Analysis (LDA) and
+    Quadratic Discriminant Analysis (QDA) depending on its argument "quadratic".
+    """
 
     def __init__(self, quadratic: bool = False):
         self._logpriors = np.empty(0)
@@ -64,7 +68,7 @@ class GaussianDiscriminantAnalysis:
             )
             self._dists.append(dist)
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> "GaussianDiscriminantAnalysis":
+    def fit(self, X: np.ndarray, y: np.ndarray) -> "GDA":
         self._check_X(X)
         self._check_y(y)
         assert X.shape[0] == y.size
@@ -95,7 +99,7 @@ class GaussianDiscriminantAnalysis:
         return preds
 
 
-def _test():
+def _test_a():
     import sklearn.metrics
     import sklearn.model_selection
     import sklearn.preprocessing
@@ -119,7 +123,7 @@ def _test():
     else:
         ref = sklearn.discriminant_analysis.LinearDiscriminantAnalysis()
 
-    model = GaussianDiscriminantAnalysis(quadratic=quadratic)
+    model = GDA(quadratic=quadratic)
 
     for train_inds, test_inds in splitter.split(X, y):
         X_train, X_test = X[train_inds, ...], X[test_inds, ...]
@@ -140,5 +144,84 @@ def _test():
     print("10-fold avg accuracy (reference):", avg_acc_ref)
 
 
+def _test_b():
+    import matplotlib.pyplot as plt
+    import sklearn.discriminant_analysis
+    import scipy.stats
+
+    def gen_data(mean, cov, cls_ind, n=75):
+        dist = scipy.stats.multivariate_normal(mean=mean, cov=cov)
+        return dist.rvs(n), np.full(n, fill_value=cls_ind, dtype=int)
+
+    def shuffle(X, y):
+        inds = np.arange(y.size)
+        np.random.shuffle(inds)
+
+        X[...] = X[inds, :]
+        y[...] = y[inds]
+
+    np.random.seed(16)
+
+    X1, y1 = gen_data([0.0, 2.0], [[1, 0.25], [0.25, 1]], 0)
+    X2, y2 = gen_data([-2.0, 2.0], [[0.5, -0.125], [-0.125, 0.5]], 1)
+    X3, y3 = gen_data([1.0, 0.2], [[0.3, -0.225], [-0.225, 0.3]], 2)
+
+    X, y = np.vstack((X1, X2, X3)), np.concatenate((y1, y2, y3))
+    shuffle(X, y)
+
+    mapper_plot = {0: (1.0, 0.8, 0.8), 1: (0.8, 0.8, 1.0), 2: (1.0, 0.95, 0.7)}
+    mapper_data = {0: (0.9, 0.07, 0.27), 1: (0.0, 0.47, 1.0), 2: (1.0, 0.8, 0.0)}
+
+    def map_color(y, mapper_color):
+        return list(map(mapper_color.get, y))
+
+    data_colors = map_color(y, mapper_data)
+
+    models = (
+        ("LDA", GDA()),
+        ("QDA", GDA(quadratic=True)),
+        ("sklearn LDA", sklearn.discriminant_analysis.LinearDiscriminantAnalysis()),
+        ("sklearn QDA", sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis()),
+    )
+
+    min_, max_ = np.quantile(X, (0, 1), axis=0)
+    t = np.linspace(min_, max_, 150)
+    plot_X, plot_Y = np.meshgrid(t[:, 0], t[:, 1], copy=False)
+    plot_data = np.column_stack((plot_X.ravel(), plot_Y.ravel()))
+
+    fig, axes = plt.subplots(2, 2, figsize=(15, 10), sharex=True, sharey=True)
+
+    def find_boundaries(plot_preds):
+        plot_preds = plot_preds.reshape(t.size // 2, t.size // 2)
+        plot_preds = plot_preds.astype(int, copy=False)
+
+        boundary_horiz = plot_preds[:, 1:] != plot_preds[:, :-1]
+        boundary_vert = plot_preds[1:, :] != plot_preds[:-1, :]
+
+        boundary_horiz = np.pad(boundary_horiz, ((0, 0), (1, 0)))
+        boundary_vert = np.pad(boundary_vert, ((1, 0), (0, 0)))
+
+        boundary = np.logical_or(boundary_horiz, boundary_vert)
+
+        return set(np.flatnonzero(boundary.ravel()))
+
+    for i, (model_name, model) in enumerate(models):
+        model.fit(X, y)
+        plot_preds = model.predict(plot_data)
+        plot_colors = map_color(plot_preds, mapper_plot)
+
+        boundary_inds = find_boundaries(plot_preds)
+        for j in boundary_inds:
+            plot_colors[j] = (0.0, 0.0, 0.0)
+
+        ax = axes[i // 2][i % 2]
+        ax.set_title(model_name)
+        ax.scatter(*plot_data.T, s=4, c=plot_colors)
+        ax.scatter(*X.T, s=4, c=data_colors)
+
+    plt.show()
+
+
 if __name__ == "__main__":
-    _test()
+    _test_a()
+    _test_b()
