b65fd2210b1c2c81beef0f36dbe26ef7b904f96c
felsiq <felipe.siqueira@usp.br>
2021-06-16 18:35:30 -0300

    Add BCELoss

--- deep-learning-algorithm-implementation/from-scratch/losses.py
+++ deep-learning-algorithm-implementation/from-scratch/losses.py
@@ -2,6 +2,9 @@ import numpy as np
 
 
 class _BaseLoss:
+    def __init__(self, average: bool = True):
+        self.average = bool(average)
+
     def __call__(self, y, y_preds):
         raise NotImplementedError
 
@@ -9,6 +12,29 @@ class _BaseLoss:
 class MSELoss(_BaseLoss):
     def __call__(self, y, y_preds):
         diff = y_preds - y
-        mse = np.sum(diff * diff) / y.size
-        grads = diff / y.size
+
+        mse = float(np.sum(diff * diff))
+        grads = diff
+
+        if self.average:
+            mse /= y.size
+            grads /= y.size
+
         return mse, grads
+
+
+class BCELoss(_BaseLoss):
+    def __call__(self, y, y_preds):
+        pos_inds = y >= 0.999
+
+        bce_loss = -float(
+            np.sum(np.log(y_preds[pos_inds])) + np.sum(np.log(1.0 - y_preds[~pos_inds]))
+        )
+
+        grads = (y_preds - y) / (y_preds * (1.0 - y_preds))
+
+        if self.average:
+            bce_loss /= y.size
+            grads /= y.size
+
+        return bce_loss, grads
