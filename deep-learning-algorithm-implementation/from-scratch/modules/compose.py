9975adf94fc4d262519a6c64fb2f1aa1deb57732
felsiq <felipe.siqueira@usp.br>
2021-06-20 12:46:22 -0300

    Simplified Sequential layer

--- deep-learning-algorithm-implementation/from-scratch/modules/compose.py
+++ deep-learning-algorithm-implementation/from-scratch/modules/compose.py
@@ -1,22 +1,10 @@
-import typing as t
-
-import numpy as np
-
 from . import base
 
 
 class Sequential(base.BaseLayer):
     def __init__(self, layers):
         super(Sequential, self).__init__(trainable=True)
-
         self.register_layers(*layers)
-
-        self.param_nums = []  # type: t.List[int]
-
-        for layer in self.layers:
-            self.param_nums.append(len(layer.parameters))
-
-        self.param_nums = tuple(self.param_nums)
 
     def forward(self, X):
         out = X
