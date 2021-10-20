9a8617dcc991720fc5db4d561250b78b62bbd7eb
felsiq <felipe.siqueira@usp.br>
2021-10-20 14:13:19 -0300

    Add K-means++ initialization

--- classical-ml-algorithm-implementation/unsupervised/py-kmeans/kmeans.py
+++ classical-ml-algorithm-implementation/unsupervised/py-kmeans/kmeans.py
@@ -3,13 +3,24 @@ import scipy.spatial
 
 
 class KMeans:
-    def __init__(self, n_clusters: int, max_iter: int = 256, metric: str = "euclidean"):
+    def __init__(
+        self,
+        n_clusters: int,
+        max_iter: int = 256,
+        metric: str = "euclidean",
+        init: str = "random",
+        init_sample_power: float = 2.0,
+    ):
         assert int(n_clusters) >= 1
         assert int(max_iter) >= 0
+        assert init in {"random", "kmeans++"}
+        assert float(init_sample_power) > 0.0
 
         self.n_clusters = int(n_clusters)
         self.max_iter = int(max_iter)
         self.metric = str(metric)
+        self.init = init
+        self.init_sample_power = float(init_sample_power)
 
         self.cluster_ids = np.empty(0, dtype=int)
         self.centroids = np.empty(0, dtype=int)
@@ -19,14 +30,43 @@ class KMeans:
         self.cluster_ids = distances.argmin(axis=1)
         return self.cluster_ids
 
+    def _init_centroids(self, X):
+        n, m = X.shape
+
+        if self.init == "random" or self.n_clusters == 1 or self.n_clusters >= n:
+            self.centroids = X[
+                np.random.choice(n, size=self.n_clusters, replace=False), :
+            ]
+            return
+
+        # K-means++ initialization
+        self.centroids = np.vstack(
+            [
+                X[np.random.choice(n, size=1), :],
+                np.empty((self.n_clusters - 1, m), dtype=float),
+            ]
+        )
+
+        centroid_min_dists = np.full(n, fill_value=np.inf, dtype=float)
+
+        for k in np.arange(1, self.n_clusters):
+            new_centroid_dist = scipy.spatial.distance.cdist(
+                X, self.centroids[k - 1, None, :], metric=self.metric
+            ).ravel()
+            np.power(new_centroid_dist, self.init_sample_power, out=new_centroid_dist)
+            np.minimum(centroid_min_dists, new_centroid_dist, out=centroid_min_dists)
+
+            sample_probs = centroid_min_dists / float(np.sum(centroid_min_dists))
+
+            new_centroid_id = np.random.choice(n, size=1, p=sample_probs)
+            self.centroids[k, :] = X[new_centroid_id, ...]
+
     def fit(self, X):
         X = np.asfarray(X)
 
         assert X.ndim == 2
 
-        n = X.shape[0]
-
-        self.centroids = X[np.random.choice(n, size=self.n_clusters, replace=False), :]
+        self._init_centroids(X)
         self.cluster_ids = self._update_clusters(X)
 
         rem_it = self.max_iter
@@ -52,19 +92,28 @@ class KMeans:
 def _test():
     import matplotlib.pyplot as plt
 
-    X = np.vstack([
-        np.random.randn(40, 2) - (2, 2),
-        4 * np.random.randn(40, 2) + (2, -5),
-        16 * np.random.randn(40, 2) + (10, 0)
-    ])
+    np.random.seed(16)
 
-    model = KMeans(n_clusters=3)
-    y_preds = model.fit_predict(X)
+    m = 5
+    fig, axes = plt.subplots(2, m, figsize=(15, 10))
 
-    colors = {0: "r", 1: "b", 2: "black"}
+    for i in range(10):
+        X = np.vstack(
+            [
+                np.random.randn(40, 2) - (2, 2),
+                4 * np.random.randn(40, 2) + (2, -5),
+                16 * np.random.randn(40, 2) + (10, 0),
+            ]
+        )
 
-    fig, ax = plt.subplots(1, figsize=(10, 10))
-    ax.scatter(*X.T, c=list(map(colors.get, y_preds)))
+        model = KMeans(n_clusters=3, init="random" if i < m else "kmeans++")
+        y_preds = model.fit_predict(X)
+        colors = {0: "r", 1: "b", 2: "black"}
+
+        ax = axes[i // m][i % m]
+        ax.scatter(*X.T, c=list(map(colors.get, y_preds)))
+        ax.set_title("K-means" if i < m else "K-means++")
+
     plt.show()
 
 
